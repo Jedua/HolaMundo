4f596d2075a102b047e5ed12450833a5d10c7b9a
Jedua <49412684+Jedua@users.noreply.github.com>
2019-04-08 13:46:35 -0500

    Create main.py
    se actualiza la salida con un mensaje nuevo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 # crear tensor
 mensaje = tf.string_join(["Hola","Mundo"])
+mensaje2 = tf.string_join(["mensaje 2"])
 #lanzar Sesion
 with tf.Session() as sess:
 	print(sess.run(mensaje))
+	print(sess.run(mensaje2))
